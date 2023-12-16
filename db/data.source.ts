ff2b0521f25795ee7bc0a0824fbb1bcd41919689
PritpalSingh <singhpritpal2%@gmail.com>
2023-12-16 16:59:57 +0530

    new one

--- db/data.source.ts
+++ db/data.source.ts
@@ -1,4 +1,5 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { UpdateSummaryListener } from '../src/tasks/update-summary.listener';
 export const dbdatasource: DataSourceOptions = {
     type: 'mssql',
     host: 'localhost',
@@ -11,6 +12,7 @@ export const dbdatasource: DataSourceOptions = {
     // entities: [__dirname + '/**/*.entity{.ts.js}'],
     entities: ['dist/**/*.entity.js'],
     migrations: ['dist/db/migrations/*.js'],
+    subscribers: [UpdateSummaryListener],
     options: { encrypt: false },
     // migrations: ['dist/tasks/migrations/*.js'],
     // migrationsTableName: "task_migrations",
--- src/tasks/update-summary.listener.ts
+++ src/tasks/update-summary.listener.ts
@@ -17,29 +17,39 @@ export class UpdateSummaryListener implements EntitySubscriberInterface<Task> {
   }
 
   afterInsert(event: InsertEvent<Task>) {
+    console.log("afterInsert triggered");
+    console.log(event.entity.user)
     this.updateSummary(event.entity.user);
   }
 
   afterUpdate(event: UpdateEvent<Task>) {
+    console.log("afterUpdate triggered");
     if (event.updatedColumns.some((column) => column.propertyName === 'user')) {
       this.updateSummary(event.entity.user);
     }
   }
 
   private async updateSummary(user: User) {
-    const summary = await this.summaryRepository.findOne({ where: { user_id: user.id } });
-  
+    console.log(user)
+    console.log("hdhhdhhd",user.id)
+    try {
+      const summary = await this.summaryRepository.findOne({ where: { user_id: user.id }});
 
-    if (summary) {
-      summary.task_count += 1; // Increment task_count by 1
-      await this.summaryRepository.save(summary);
-    } else {
-      const newSummary = this.summaryRepository.create({
-        user_id: user.id,
-        username: user.username,
-        task_count: 1, // Assuming a new task is being created
-      });
-      await this.summaryRepository.save(newSummary);
+      if (summary) {
+        summary.task_count += 1; // Increment task_count by 1
+        await this.summaryRepository.save(summary);
+        console.log("Summary updated:", summary);
+      } else {
+        const newSummary = this.summaryRepository.create({
+          user_id: user.id,
+          username: user.username,
+          task_count: 1, // Assuming a new task is being created
+        });
+        await this.summaryRepository.save(newSummary);
+        console.log("New summary created:", newSummary);
+      }
+    } catch (error) {
+      console.error("Error updating summary:", error);
     }
   }
 }
